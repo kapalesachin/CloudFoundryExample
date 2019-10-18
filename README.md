0698a4d04dfd4f63a0c990002937dcc2e59097e6
kapalesachin <30473300+kapalesachin@users.noreply.github.com>
2019-10-18 00:20:49 -0400

    Added the files
    Initial files with licensing information

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# CloudFoundryExample
- Cloudfoundry Hello World Example
+Hello Spring Cloud Connectors
+============
+
+## Building the application
+
+Use Maven to build the application:
+
+~~~
+$ mvn clean package
+~~~
+
+## Running the application on Cloud Foundry
+
+To run the application on Cloud Foundry, first target and long into a Cloud Foundry environment, then run this command:
+
+~~~
+$ cf push
+~~~
+
+The application will be deployed using settings in the provided `manifest.yml` file. The output from the command will show the URL that has been assigned to the application. Browse to the provided URL to view information about the application.
+
+### Creating and binding services
+
+Using the provided manifest, the application will not be bound to any data services. The application UI will show default connections provided by Spring Boot. You can create relational database, Redis, MongoDB, and AMQP services and bind them to the application to test creation of service connections with Spring Cloud Connectors.
+
+On [Pivotal Web Services](https://run.pivotal.io/) you can create and bind each type of service using these commands:
+
+~~~
+$ cf create-service cleardb spark mysql-service
+$ cf bind-service hello-spring-cloud mysql-service
+
+
+$ cf restart
+~~~
---
+++ src/main/java/helloworld/HomeController.java
@@ -0,0 +1,72 @@
+package helloworld;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.app.ApplicationInstanceInfo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @Autowired(required = false) DataSource dataSource;
+    @Autowired(required = false) ApplicationInstanceInfo instanceInfo;
+
+    @RequestMapping("/")
+    public String home(Model model) {
+        model.addAttribute("instanceInfo", instanceInfo);
+
+        if (instanceInfo != null) {
+            Map<Class<?>, String> services = new LinkedHashMap<Class<?>, String>();
+            services.put(dataSource.getClass(), toString(dataSource));
+            model.addAttribute("services", services.entrySet());
+        }
+
+        return "home";
+    }
+
+    private String toString(DataSource dataSource) {
+        if (dataSource == null) {
+            return "<none>";
+        } else {
+            try {
+                Field urlField = ReflectionUtils.findField(dataSource.getClass(), "url");
+                ReflectionUtils.makeAccessible(urlField);
+                return stripCredentials((String) urlField.get(dataSource));
+            } catch (Exception fe) {
+                try {
+                    Method urlMethod = ReflectionUtils.findMethod(dataSource.getClass(), "getUrl");
+                    ReflectionUtils.makeAccessible(urlMethod);
+                    return stripCredentials((String) urlMethod.invoke(dataSource, (Object[])null));
+                } catch (Exception me){
+                    return "<unknown> " + dataSource.getClass();
+                }
+            }
+        }
+    }
+
+   
+
+    private String stripCredentials(String urlString) {
+        try {
+            if (urlString.startsWith("jdbc:")) {
+                urlString = urlString.substring("jdbc:".length());
+            }
+            URI url = new URI(urlString);
+            return new URI(url.getScheme(), null, url.getHost(), url.getPort(), url.getPath(), null, null).toString();
+        }
+        catch (URISyntaxException e) {
+            System.out.println(e);
+            return "<bad url> " + urlString;
+        }
+    }
+
+}
